49819b681b5a9c4cfe79d9fc96b506d575cfb05c
MonoxideCarbon <yajasvirat@gmail.com>
2026-08-16 16:35:55 +0530

    Fix Edge auth TypeScript build error

--- lib/auth-edge.ts
+++ lib/auth-edge.ts
@@ -16,7 +16,7 @@ function base64UrlDecodeText(str: string): string {
 function base64UrlEncode(input: ArrayBuffer): string {
   const bytes = new Uint8Array(input)
   let binary = ''
-  for (const byte of bytes) binary += String.fromCharCode(byte)
+  for (let i = 0; i < bytes.length; i++) binary += String.fromCharCode(bytes[i])
   return btoa(binary).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
 }
 
