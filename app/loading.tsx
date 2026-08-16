97994915716d7fb1d1ff6bab02c8f0169c3ff8fe
MonoxideCarbon <yajasvirat@gmail.com>
2026-08-16 19:47:04 +0530

    Add minimal global loading screen

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingMark from '@/components/ui/LoadingMark'
+
+export default function Loading() {
+  return (
+    <main className="fixed inset-0 grid place-items-center bg-white dark:bg-carbon-950">
+      <LoadingMark size={48} />
+    </main>
+  )
+}
